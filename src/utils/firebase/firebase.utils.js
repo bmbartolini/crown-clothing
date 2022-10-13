84ad79f8bd8bad5cbb0074f56d728a4bfe093139
Brian Bartolini <brian.bartolini8@gmail.com>
2022-10-12 20:27:03 -0700

    added sign in component and firbase uils and database

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,72 @@
+import { initializeApp} from "firebase/app";
+import { 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider} 
+  from "firebase/auth";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  setIndexConfiguration
+} from "firebase/firestore"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKmzNY25wHUGzRAHKz_-zHJercDEddwG8",
+    authDomain: "crwn-clothing-db-e82c2.firebaseapp.com",
+    projectId: "crwn-clothing-db-e82c2",
+    storageBucket: "crwn-clothing-db-e82c2.appspot.com",
+    messagingSenderId: "1094258198173",
+    appId: "1:1094258198173:web:6c69abf11d3b08ec657674"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+    prompt: "select_account"
+  })
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log(userDocRef);
+  
+    const userSnapShot = await getDoc(userDocRef);
+    console.log(userSnapShot);
+
+    console.log(userSnapShot.exists())
+
+    if(!userSnapShot.exists()){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+    
+      try {
+        await setDoc(userDocRef, {displayName, email, createdAt});
+
+      }catch(error){
+        console.log("error created the user", error.message);
+      }
+    
+      return userDocRef;
+    }
+    
+    //if user data does not exits
+    //create set document with data from userAuth in collection
+
+    //if user date exists-
+    //create set document with data from userAuth in collection
+
+    
+  }
+
